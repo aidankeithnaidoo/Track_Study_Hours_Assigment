a96582316346ba8a139258dd34a0ea2698694d3d
aidan <aidan@AIDANKIETHPC>
2023-08-17 18:52:24 +0200

    Add project files.

---
+++ ST10109482_Prog2B/Setup/Module.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10109482_Prog2B.Setup
+{
+    public class Module
+    {
+        private string moduleCode;
+        private string moduleName;
+        private int credits;
+        private double classHours;
+        private int semsterWeeks;
+        private string startDate;
+
+        private List<Module> moduleList = new List<Module>();
+
+        public Module(string moduleCode, string moduleName, int credits, double classHours, int semsterWeeks, string startDate)
+        {
+            this.ModuleCode = moduleCode;
+            this.ModuleName = moduleName;
+            this.Credits = credits;
+            this.ClassHours = classHours;
+            this.SemsterWeeks = semsterWeeks;
+            this.StartDate = startDate;
+        }
+
+        public string ModuleCode { get => moduleCode; set => moduleCode = value; }
+        public string ModuleName { get => moduleName; set => moduleName = value; }
+        public int Credits { get => credits; set => credits = value; }
+        public double ClassHours { get => classHours; set => classHours = value; }
+        public int SemsterWeeks { get => semsterWeeks; set => semsterWeeks = value; }
+        public string StartDate { get => startDate; set => startDate = value; }
+    }
+}
---
+++ ST10109482_Prog2B/Windows/captureWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ST10109482_Prog2B.Windows
+{
+    /// <summary>
+    /// Interaction logic for captureWindow.xaml
+    /// </summary>
+    public partial class captureWindow : Window
+    {
+        public captureWindow()
+        {
+            InitializeComponent();
+        }
+        private void close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
